561eca52e728c2dc617e6495ff172ae018de1e83
thangbeo2812 <namthangmu123@gmail.com>
2021-02-06 15:05:33 +0700

    Thang dep lam

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,23 @@
+/*-------------------- About section tabs --------------------*/
+(() =>
+{
+    const aboutSection = document.querySelector(".about-section");
+    tabsContainer = document.querySelector(".about-tabs");
+    tabsContainer.addEventListener("click", (event) => 
+    {
+        // console.log(event.target);
+        if(event.target.classList.contains("tab-item") && !event.target.classList.contains("active"))
+        {
+            const target = event.target.getAttribute("data-target");
+            tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+            event.target.classList.add("outer-shadow", "active");
+
+            aboutSection.querySelector(".tab-content.active").classList.remove("active");
+            aboutSection.querySelector(target).classList.add("active");
+        }
+    });
+})();   
+
+
+
+
