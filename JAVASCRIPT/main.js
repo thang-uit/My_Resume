644ced5bc0d31d20372f4c50c6df0cdc891c6c55
thangbeo2812 <namthangmu123@gmail.com>
2021-02-16 16:22:06 +0700

    Mung 5 tet

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -23,5 +23,37 @@
 {
     const filterContainer = document.querySelector(".portfolio-filter"),
     portfolioItemsContainer = document.querySelector(".portfolio-items"),
-    portfolioItems = document.querySelectorAll(".portfolio-item");
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    popup = document.querySelector(".portfolio-popup"),
+    prevBtn = popup.querySelector(".pp-prev"),
+    nextBtn = popup.querySelector(".pp-next"),
+    closeBtn = popup.querySelector(".pp-close"),
+    projectDetailsContainer = popup.querySelector(".pp-details"),
+    projectDetailsBtn = popup.querySelector(".pp-project-details-btn");
+
+    let itemIndex, slideIndex, screenshots;
+    filterContainer.addEventListener("click", (event)=>
+    {
+        if(event.target.classList.contains("filter-item") && !event.target.classList.contains("active"))
+        {
+            filterContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+            event.target.classList.add("outer-shadow", "active");
+            
+            const target = event.target.getAttribute("data-target");
+            portfolioItems.forEach((item) =>
+            {
+                if(target === item.getAttribute("data-category") || target === 'all')
+                {
+                    item.classList.remove("hide");
+                    item.classList.add("show");
+                }
+                else
+                {
+                    item.classList.remove("show");
+                    item.classList.add("hide");
+                }
+            })
+        }
+    });
+
 })();   
