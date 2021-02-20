73d77c7efa92b13c3caae9ed3402d63ab7fc6fdc
thangbeo2812 <namthangmu123@gmail.com>
2021-02-20 23:46:09 +0700

    Mung 9 tet

--- JAVASCRIPT/style-switcher.js
+++ JAVASCRIPT/style-switcher.js
@@ -39,8 +39,26 @@ function SetActiveStyle(color)
 
 
 
-/*-------------------- Theme Light/Dark Mode --------------------*/
+/*-------------------- Light/Dark Mode --------------------*/
+const dayNight = document.querySelector(".day-night");
 
+window.addEventListener("load", () =>
+{
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+});
+
+dayNight.addEventListener("click", () =>
+{
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    document.body.classList.toggle("dark");
+});
 
 
 
